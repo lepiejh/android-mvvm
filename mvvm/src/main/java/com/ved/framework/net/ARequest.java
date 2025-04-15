8f516ee9bd2b9750e286087922fbf7834b9e9d29
lixiong <qqlixiongjob@163.com>
2025-04-15 17:12:33 +0800

    ......

--- mvvm/src/main/java/com/ved/framework/net/ARequest.java
+++ mvvm/src/main/java/com/ved/framework/net/ARequest.java
@@ -17,41 +17,52 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 /**
  * 网络请求
  *
  * @param <T> service interface
  * @param <K> 返回的数据类型
+ *
+ * 使用生命周期事件流
+ * val lifecycleDisposable = PublishSubject.create<Unit>()
+ *
+ * 当需要取消时
+   fun cancelByLifecycle(){
+        lifecycleDisposable.onNext(Unit)
+       }
+ *
  */
 public abstract class ARequest<T, K> {
 
     /**
      * 可自定义code 封装处理    继承 ApiDisposableObserver
      */
-    public void request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, @Nullable IResponse<K> iResponse) {
-        request(activity, viewModel, service, method, 0, iResponse);
+    public PublishSubject<Object> request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, @Nullable IResponse<K> iResponse) {
+        return request(activity, viewModel, service, method, 0, iResponse);
     }
 
-    public void request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, boolean isLoading, @Nullable IResponse<K> iResponse) {
-        request(activity, viewModel, service, method, 0, isLoading, iResponse);
+    public PublishSubject<Object> request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, boolean isLoading, @Nullable IResponse<K> iResponse) {
+        return request(activity, viewModel, service, method, 0, isLoading, iResponse);
     }
 
-    public void request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, int index, @Nullable IResponse<K> iResponse) {
-        request(activity, viewModel, method, service,index,false,iResponse);
+    public PublishSubject<Object> request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, int index, @Nullable IResponse<K> iResponse) {
+        return request(activity, viewModel, method, service,index,false,iResponse);
     }
 
-    public void request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, int index, boolean isLoading, @Nullable IResponse<K> iResponse) {
-        request(activity, viewModel, method, service,index,isLoading,iResponse);
+    public PublishSubject<Object> request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method, int index, boolean isLoading, @Nullable IResponse<K> iResponse) {
+        return request(activity, viewModel, method, service,index,isLoading,iResponse);
     }
 
-    public void request(boolean isLoading,@Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method,  View view,ISeatSuccess seatSuccess,ISeatError seatError, @Nullable IResponse<K> iResponse) {
-        request(isLoading,viewModel, method, service,view,seatSuccess,seatError,iResponse);
+    public PublishSubject<Object> request(boolean isLoading,@Nullable BaseViewModel viewModel, @Nullable Class<? extends T> service, @Nullable IMethod<T, K> method,  View view,ISeatSuccess seatSuccess,ISeatError seatError, @Nullable IResponse<K> iResponse) {
+        return request(isLoading,viewModel, method, service,view,seatSuccess,seatError,iResponse);
     }
 
 
     @SuppressLint("CheckResult")
-    public void request(boolean isLoading,@Nullable BaseViewModel viewModel,@Nullable IMethod<T, K> method,@Nullable Class<? extends T> service,View view,ISeatSuccess seatSuccess,ISeatError seatError,@Nullable IResponse<K> iResponse) {
+    public PublishSubject<Object> request(boolean isLoading,@Nullable BaseViewModel viewModel,@Nullable IMethod<T, K> method,@Nullable Class<? extends T> service,View view,ISeatSuccess seatSuccess,ISeatError seatError,@Nullable IResponse<K> iResponse) {
+        PublishSubject<Object> lifecycleDisposable = PublishSubject.create();
         if (view!= null) {
             seatSuccess.onStateView();
         }
@@ -97,12 +108,14 @@ public abstract class ARequest<T, K> {
                                         parseError( isLoading,viewModel,view,seatError,msg[0], iResponse);
                                         return Observable.error(throwable);
                                     }))
+                            .takeUntil(lifecycleDisposable)
                             .subscribe((Consumer<K>) response -> parseSuccess(isLoading,viewModel,view, iResponse, response),(Consumer<ResponseThrowable>) throwable -> parseError( isLoading,viewModel,view,seatError, iResponse, throwable));
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+        return lifecycleDisposable;
     }
 
 
@@ -174,7 +187,8 @@ public abstract class ARequest<T, K> {
     }
 
     @SuppressLint("CheckResult")
-    public void request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable IMethod<T, K> method,@Nullable Class<? extends T> service,int index,boolean isLoading, @Nullable IResponse<K> iResponse) {
+    public PublishSubject<Object> request(@Nullable Activity activity, @Nullable BaseViewModel viewModel, @Nullable IMethod<T, K> method,@Nullable Class<? extends T> service,int index,boolean isLoading, @Nullable IResponse<K> iResponse) {
+        PublishSubject<Object> lifecycleDisposable = PublishSubject.create();
         if (isLoading && viewModel != null) {
             viewModel.showDialog();
         }
@@ -205,12 +219,14 @@ public abstract class ARequest<T, K> {
                                         parseError(viewModel, isLoading,msg[0], iResponse);
                                         return Observable.error(throwable);
                                     }))
+                            .takeUntil(lifecycleDisposable)
                             .subscribe((Consumer<K>) response -> parseSuccess(viewModel, isLoading, iResponse, response),(Consumer<ResponseThrowable>) throwable -> parseError(viewModel, isLoading, iResponse, throwable, activity));
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+        return lifecycleDisposable;
     }
 
     private void parseError(@Nullable BaseViewModel viewModel, boolean isLoading, String msg,IResponse<K> iResponse) {
