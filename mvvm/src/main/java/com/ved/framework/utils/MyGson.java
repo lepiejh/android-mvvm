133d8344392b255ca256c4105d68eb2662fe7140
lixiong <qqlixiongjob@163.com>
2025-04-16 13:36:25 +0800

    ......

--- mvvm/src/main/java/com/ved/framework/utils/MyGson.java
+++ mvvm/src/main/java/com/ved/framework/utils/MyGson.java
@@ -1,10 +1,7 @@
 package com.ved.framework.utils;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.annotations.Expose;
 
 public class MyGson {
     private MyGson() {
@@ -19,18 +16,6 @@ public class MyGson {
     }
 
     public Gson getGson(){
-        return new GsonBuilder().setExclusionStrategies(new ExclusionStrategy() {
-            @Override
-            public boolean shouldSkipField(FieldAttributes f) {
-                // 如果字段有@Expose注解且值为null，则跳过
-                return f.getAnnotation(Expose.class) != null &&
-                        !f.getAnnotation(Expose.class).serialize();
-            }
-
-            @Override
-            public boolean shouldSkipClass(Class<?> clazz) {
-                return false;
-            }
-        }).registerTypeAdapterFactory(new NullStringToEmptyAdapterFactory()).create();
+        return new GsonBuilder().registerTypeAdapterFactory(new NullStringToEmptyAdapterFactory()).create();
     }
 }
