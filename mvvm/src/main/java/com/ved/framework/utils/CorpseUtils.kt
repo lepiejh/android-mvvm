8f2569593e00355e2b9d7e18334cd9545cd53449
lixiong <qqlixiongjob@163.com>
2025-04-21 13:43:06 +0800

    ......

--- mvvm/src/main/java/com/ved/framework/utils/CorpseUtils.kt
+++ mvvm/src/main/java/com/ved/framework/utils/CorpseUtils.kt
@@ -165,15 +165,13 @@ object CorpseUtils {
         }
     }
 
+    @PublishedApi
     internal inline fun <reified T : Any> noOpDelegate(): T {
         val javaClass = T::class.java
         return Proxy.newProxyInstance(
-            javaClass.classLoader, arrayOf(javaClass), NO_OP_HANDLER
-        ) as T
-    }
-
-    private val NO_OP_HANDLER = InvocationHandler { _, _, _ ->
-        // no op
+            javaClass.classLoader, arrayOf(javaClass)
+        ) { _, _, _ ->
+        } as T
     }
 
     fun inspectRequestBody(request: Request) {
