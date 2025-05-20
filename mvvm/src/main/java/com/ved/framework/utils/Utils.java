b5dd9775f39cea14b79c4a803b614ad934aec79b
lixiong <qqlixiongjob@163.com>
2025-05-20 13:46:20 +0800

    ......

--- mvvm/src/main/java/com/ved/framework/utils/Utils.java
+++ mvvm/src/main/java/com/ved/framework/utils/Utils.java
@@ -14,6 +14,7 @@ public final class Utils {
 
     @SuppressLint("StaticFieldLeak")
     private static Context context;
+    private static Application app;
 
     private Utils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
@@ -25,8 +26,16 @@ public final class Utils {
      * @param context 上下文
      */
     public static void init(@NonNull final Application context) {
+        Utils.app = context;
         Utils.context = context.getApplicationContext();
         com.ved.framework.utils.bland.code.Utils.init(context);
+    }
+
+    public static Application getApplication(){
+        if (app != null) {
+            return app;
+        }
+        throw new NullPointerException("should be initialized in application");
     }
 
     /**
