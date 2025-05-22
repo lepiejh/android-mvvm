93e643a1a51f812070499ef71b820d11ac7d3311
lixiong <qqlixiongjob@163.com>
2025-05-22 10:56:33 +0800

    ......

--- mvvm/src/main/java/com/ved/framework/base/ImmersionBarBaseActivity.java
+++ mvvm/src/main/java/com/ved/framework/base/ImmersionBarBaseActivity.java
@@ -1,22 +1,96 @@
 package com.ved.framework.base;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 
-import androidx.annotation.Nullable;
-
 import com.gyf.immersionbar.BarHide;
 import com.gyf.immersionbar.ImmersionBar;
+import com.trello.rxlifecycle4.components.support.RxAppCompatActivity;
 import com.ved.framework.R;
+import com.ved.framework.utils.KLog;
+import com.ved.framework.utils.ScreenUtils;
+import com.ved.framework.utils.UIUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import androidx.annotation.Nullable;
 
 public class ImmersionBarBaseActivity extends RxAppCompatActivity implements ViewTreeObserver.OnGlobalLayoutListener{
     private ImmersionBar mImmersionBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        //为了解决Android自level 1以来的[安装完成点击“Open”后导致的应用被重复启动]的Bug
+        if (!isTaskRoot()){
+            Intent intent = getIntent();
+            String intentAction = intent.getAction();
+            if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && intentAction != null && UIUtils.equals(intentAction,Intent.ACTION_MAIN)){
+                finish();
+            }
+        }
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+//            HookSystem.Companion.getInstance().hookSystemHandler();
+        }else {
+            //如果是8.0系统的手机，并且认为是透明主题的Activity
+            if (Build.VERSION.SDK_INT == 26 && this.isTranslucentOrFloating()) {
+                //通过反射取消方向的设置，这样绕开系统的检查，避免闪退
+                boolean result = this.fixOrientation();
+                KLog.i("only_opaque","result : "+result);
+            }
+        }
         super.onCreate(savedInstanceState);
+        if (isRegular()) {
+            if (!ScreenUtils.isTabletDevice()){
+                if (getRequestedOrientation() == android.content.pm.ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+            }
+        }
         initStatusBar();
+    }
+
+    public boolean isRegular(){
+        return true;
+    }
+
+    //通过反射判断是否是透明页面
+    private boolean isTranslucentOrFloating() {
+        boolean isTranslucentOrFloating = false;
+        try {
+            int[] styleableRes = (int[]) Class.forName("com.android.internal.R$styleable").getField("Window").get(null);
+            final TypedArray ta = obtainStyledAttributes(styleableRes);
+            Method m = ActivityInfo.class.getMethod("isTranslucentOrFloating", TypedArray.class);
+            m.setAccessible(true);
+            isTranslucentOrFloating = (boolean) m.invoke(null, ta);
+            m.setAccessible(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isTranslucentOrFloating;
+    }
+
+    //通过反射将方向设置为 SCREEN_ORIENTATION_UNSPECIFIED，绕开系统的检查
+    private boolean fixOrientation() {
+        try {
+            Field field = Activity.class.getDeclaredField("mActivityInfo");
+            field.setAccessible(true);
+            ActivityInfo o = (ActivityInfo) field.get(this);
+            if (o != null) {
+                o.screenOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+            }
+            field.setAccessible(false);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     /**
