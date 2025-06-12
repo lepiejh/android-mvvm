4acb9b8c9047da17da1caa6a02746279501fbe19
lixiong <qqlixiongjob@163.com>
2025-06-12 09:56:37 +0800

    ......

--- mvvm/src/main/java/com/ved/framework/base/BasePageAdapter.java
+++ mvvm/src/main/java/com/ved/framework/base/BasePageAdapter.java
@@ -1,23 +1,19 @@
 package com.ved.framework.base;
 
 import android.view.ViewGroup;
-
-import com.ved.framework.utils.KLog;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
-public class BasePageAdapter extends FragmentPagerAdapter {
+public class BasePageAdapter extends FragmentStatePagerAdapter {
     private final List<Fragment> fragments = new ArrayList<>();
     private final List<String> mTitles = new ArrayList<>();
 
-    public void addTitle(String title){
+    public void addTitle(String title) {
         mTitles.add(title);
     }
 
@@ -31,14 +27,13 @@ public class BasePageAdapter extends FragmentPagerAdapter {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
     }
 
-    public void addFragment(Fragment fragment){
+    public void addFragment(Fragment fragment) {
         fragments.add(fragment);
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        KLog.i("BasePageAdapter","getItem -> position : "+position);
         return fragments.get(position);
     }
 
@@ -50,7 +45,6 @@ public class BasePageAdapter extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        KLog.i("BasePageAdapter","instantiateItem -> position : "+position);
         return super.instantiateItem(container, position);
     }
 }
