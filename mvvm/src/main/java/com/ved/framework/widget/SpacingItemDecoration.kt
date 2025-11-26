a025ad60f022e49434e98eab31ba45eddc811e19
dwden <dw.deng@163.com>
2025-11-26 09:10:53 +0800

    ...

--- mvvm/src/main/java/com/ved/framework/widget/SpacingItemDecoration.kt
+++ mvvm/src/main/java/com/ved/framework/widget/SpacingItemDecoration.kt
@@ -1,17 +1,10 @@
 package com.ved.framework.widget
 
-import android.content.Context
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.ved.framework.utils.DpiUtils
 
-/**
- *@Des   syyk_android
- *
- *@Time 2023/10/17
- *
- *@Author  Peng-BinBin
- **/
 class SpacingItemDecoration  (private val spacing: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(
@@ -22,18 +15,11 @@ class SpacingItemDecoration  (private val spacing: Int) : RecyclerView.ItemDecor
     ) {
         super.getItemOffsets(outRect, view, parent, state)
 
-        val spacingPx = dpToPx(spacing, parent.context)
+        val spacingPx = DpiUtils.dip2px(parent.context,spacing)
         // 设置左、上、右、下四个方向的间距（这里统一设置为spacingPx的值）
         outRect.left = spacingPx
         outRect.right = spacingPx
         outRect.top = spacingPx
         outRect.bottom = spacingPx
     }
-
-    private fun dpToPx(dp: Int, context: Context): Int {
-        val density = context.resources.displayMetrics.density
-        return (dp * density).toInt()
-    }
-
-
 }
